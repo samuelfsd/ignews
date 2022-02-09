b22dfe24de32ce378c0d05c24dbcb5ca27942bbe
Samuel Alves <samuel.estudo778@gmail.com>
2022-02-09 11:37:47 -0300

    feat: add SubscribeButton

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import Head from 'next/head';
 import Image from 'next/image';
+import { SubscribeButton } from '../components/SubscribeButton';
 
 import styles from './home.module.scss';
 
@@ -17,6 +18,7 @@ export default function Home() {
             Get acess to all the publications <br />
             <span>for $9.90 month</span> 
           </p>
+          <SubscribeButton />
         </section>
 
         <Image src="/images/avatar.svg" alt="Girl coding" width={340} height={520} />
